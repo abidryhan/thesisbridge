43f5d035e3054cfffd5df80852845f61fcd8c5a3
Abid <abid.ryhan@g.bracu.ac.bd>
2026-08-11 21:54:14 +0600

    modified milestone.php, added document model and migration

---
+++ app/Models/Document.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Document extends Model
+{
+    protected $fillable = [
+        'milestone_id',
+        'user_id',
+        'file_path',
+        'original_filename',
+        'version_number',
+    ];
+
+    public function milestone(): BelongsTo
+    {
+        return $this->belongsTo(Milestone::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
