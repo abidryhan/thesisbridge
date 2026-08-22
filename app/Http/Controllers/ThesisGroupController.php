ee7f707b4c92e1e6e1a0befe40a4eca12595e1e4
jobayeralam439 <120060821+jobayeralam439@users.noreply.github.com>
2026-08-23 00:31:25 +0600

    Add supervised() action showing ghost-flagged groups for the logged-in supervisor

--- app/Http/Controllers/ThesisGroupController.php
+++ app/Http/Controllers/ThesisGroupController.php
@@ -181,5 +181,25 @@ class ThesisGroupController extends Controller
         if (!$isMember) {
             abort(403);
         }
+    }public function supervised(): View
+{
+    $supervisor = auth()->user()->supervisor;
+
+    if (!$supervisor) {
+        abort(403);
     }
+
+    $groups = $supervisor->thesisGroups()
+        ->with('students.user')
+        ->latest()
+        ->get()
+        ->map(function (ThesisGroup $group) {
+            $group->daysSinceLastActivity = $group->daysSinceLastActivity();
+            $group->isGhost = $group->daysSinceLastActivity > config('thesisbridge.ghost_threshold_days');
+
+            return $group;
+        });
+
+    return view('thesis-groups.supervised', ['groups' => $groups]);
+}
 }
